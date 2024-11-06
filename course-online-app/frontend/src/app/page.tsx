dc7ce71dc697804be2f5536db9883710417e4afa
Maycon Pires <piresmaycon4@gmail.com>
2024-11-06 15:56:08 -0300

    erro de criação

--- course-online-app/frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductList from "@/components/ProductList";
-
-export default function Home() {
-  return (
-    <div className="container mx-auto p-4">
-      <ProductList />
-    </div>
-  );
-}
